29cf35ec8f3ab3466c6457a25abb895f3d842ac9
Carlos Barrera <carlostonatihu@gmail.com>
2017-08-22 16:58:31 -0500

    Subir varios archivos

--- Practica1/src/interfaz/Practica1.java
+++ Practica1/src/interfaz/Practica1.java
@@ -5,8 +5,11 @@
  */
 package interfaz;
 
+import javax.swing.*;
+import java.io.*;
+import java.net.Socket;
+
 /**
- *
  * @author tona
  */
 public class Practica1 {
@@ -16,6 +19,52 @@ public class Practica1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Envia iniciado");
+        JFileChooser jf = new JFileChooser();
+        jf.setMultiSelectionEnabled(true);
+        int r = jf.showOpenDialog(null);
+        if (r == JFileChooser.APPROVE_OPTION) {
+            String host = "localhost";
+            int pto = 9999;
+            int n;
+
+            File files[] = jf.getSelectedFiles();
+            for (File f : files) {
+                String nombre = f.getName();
+                long tam = f.length();
+                long enviados = 0;
+                int porcentaje;
+
+                String ruta = f.getAbsolutePath();
+                try {
+                    Socket cl = new Socket(host, pto);
+                    System.out.println("Conexion establecida");
+                    DataOutputStream dos = new DataOutputStream(cl.getOutputStream());
+                    DataInputStream dis = new DataInputStream(new FileInputStream(ruta));
+                    dos.writeUTF(nombre);
+                    dos.flush();
+                    dos.writeLong(tam);
+                    dos.flush();
+                    System.out.println("Enviando el archivo: " + nombre);
+                    while (enviados < tam) {
+                        byte[] b = new byte[1500];
+                        n = dis.read(b);
+                        System.out.println("La otra n: " + n);
+                        dos.write(b, 0, n);
+                        dos.flush();
+                        enviados = enviados + n;
+                        porcentaje = (int) (enviados * 100 / tam);
+                        System.out.println("\rSe ha transmitido el: " + porcentaje + "%");
+                    }
+                    System.out.println("Archivo enviado");
+                    cl.close();
+                    dos.close();
+                    dis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
-    
+
 }
--- Practica1/src/sockets/Servidor.java
+++ Practica1/src/sockets/Servidor.java
@@ -5,10 +5,51 @@
  */
 package sockets;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  *
  * @author tona
  */
 public class Servidor {
-    
+    public static void main(String[] args) {
+        int puerto = 9999;
+        int n;
+        try {
+            ServerSocket s;
+            s = new ServerSocket(puerto);
+            s.setReuseAddress(true);
+            System.out.println("Servicio iniciado...");
+            for(;;) {
+                System.out.println("Eperando conexion...");
+                Socket cl = s.accept();
+                System.out.println("Cliente conectado desde: " + cl.getInetAddress() + " : " + cl.getPort());
+                DataInputStream dis = new DataInputStream(cl.getInputStream());
+                String nombre = dis.readUTF();
+                long tam = dis.readLong();
+                DataOutputStream dos = new DataOutputStream(new FileOutputStream(nombre));
+                long recibidos = 0;
+                System.out.println("Escribiendo el archivo: " + nombre);
+                while (recibidos<tam) {
+                    byte[] b = new byte[1500];
+                    n = dis.read(b);
+                    System.out.println("El valor de n: " + n);
+                    dos.write(b, 0, n);
+                    dos.flush();
+                    recibidos = recibidos + n;
+                }
+                System.out.println("Archivo recibido");
+                dos.close();
+                dis.close();
+                cl.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
