1761aaf78797dc3a8223636f1a41dbfbb7d6fef0
Tonatihu <carlostonatihu@gmail.com>
2017-10-29 21:44:20 -0600

    En teoria ya esta el servidor del ahorcado

--- ahorcado/Cliente/src/cliente/Cliente.java
+++ ahorcado/Cliente/src/cliente/Cliente.java
@@ -5,18 +5,33 @@
  */
 package cliente;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 /**
  *
  * @author tona
  */
 public class Cliente {
-
+    static final int SOLICITAR_PALABRA = 1;
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
         System.out.println("Cliente");
+        Socket cliente = new Socket("localhost", 7777);
+        DataOutputStream dos = new DataOutputStream(cliente.getOutputStream());
+        DataInputStream dis = new DataInputStream(cliente.getInputStream());
+        dos.write(SOLICITAR_PALABRA+'0');
+        byte palabra[] = new byte[1200];
+        int n = dis.read(palabra);
+        System.out.println(new String(palabra, 0, n));
+        dos.close();
+        dis.close();
+        cliente.close();
     }
     
 }
